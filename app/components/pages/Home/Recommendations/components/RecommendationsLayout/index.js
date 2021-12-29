2483c01067cce45d2a2c54c1f1b247a482cd7dd5
Nastik1811 <lugovskaya.1999@mail.ru>
2021-12-30 02:32:11 +0300

    feat: add styles for recommendations and menu sections

--- app/components/pages/Home/Recommendations/components/RecommendationsLayout/index.js
+++ app/components/pages/Home/Recommendations/components/RecommendationsLayout/index.js
@@ -1,28 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Wrapper from "../../../../../common/Wrapper";
-import ProductCard from "../../../../../common/productCard";
-import { Grid } from "@material-ui/core";
+import ProductCard from "../../../../../common/ProductCard";
+import styles from "./styles.module.scss"
 import { data } from "./data";
 import { v4 as uuid4 } from "uuid";
 const RecommendationsLayout = () => {
   return (
     <Wrapper style={{ marginBottom: 91 }}>
-      <Grid container spacing={0}>
-        <Grid item xs={3} style={{ height: 419 }}>
+      <div className={styles.grid}>
+        <h2 className={styles.title}>
           Рекомендуемые товары 2021
-        </Grid>
+        </h2>
         {data.map((card) => (
-          <Grid item xs={3} key={uuid4()}>
-            <ProductCard
-              productName={card.productName}
-              price={card.price}
-              img={card.img}
-              alt={card.alt}
-            />
-          </Grid>
+          <ProductCard
+            productName={card.productName}
+            price={card.price}
+            img={card.img}
+            alt={card.alt}
+            key={uuid4()}
+          />
         ))}
-      </Grid>
+      </div>
     </Wrapper>
   );
 };
