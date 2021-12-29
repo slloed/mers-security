758b2c2555951542017f84eaa9cd45d7011372e9
Nastik1811 <lugovskaya.1999@mail.ru>
2021-12-30 00:39:48 +0300

    Add about page

---
+++ app/pages/about/index.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Wrapper from "../../components/common/Wrapper";
+import { about, philosophy, storyPoints } from "./data";
+import picture from "../../assets/images/about.jpg"
+import picture2 from "../../assets/images/about2.jpg"
+import styles from "./styles.module.scss"
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h2 className={styles.heading}>О компании “Мерс Секьюрити”</h2>
+      <section className={styles.section}>
+        <Image src={picture} alt="logo" width="700" height="460" />
+        <h3 className={styles["section-heading"]}>О компании</h3>
+        {about.map(paragraph =>
+          <div>
+            {paragraph}
+          </div>
+        )}
+      </section>
+      <section className={styles.section}>
+        <Image src={picture2} alt="logo" width="700" height="460" />
+        <h3 className={styles["section-heading"]}>Философия компании</h3>
+
+        {philosophy.map(paragraph =>
+          <div>
+            {paragraph}
+          </div>
+        )}
+
+      </section>
+      <section className={styles["history-section"]}>
+        <h3 className={styles["section-heading"]}>История развития компании</h3>
+        <div className={styles["history-grid"]}>
+          {
+            storyPoints.map(point =>
+              <div className={styles["history-point"]}>
+                <div className={styles.year}>{point.year}</div>
+                <div className={styles.description}>{point.description}</div>
+              </div>
+            )
+          }
+        </div>
+      </section>
+    </Wrapper >
+  )
+};
+
+export default About;
